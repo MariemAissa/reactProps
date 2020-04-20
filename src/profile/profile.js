1f7d96ca668cd737ad47960b9cc2ed7d2a3f8c1f
Mariem Aissa <maissa@equalios.com>
2020-04-20 16:54:07 +0200

    last update

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -2,25 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import  './profile.css'
 
+
 const Profile=(props)=>{
-    console.log(props.data)
     const handleName=(props)=>{
         alert("User Name: "+props);
     }
+            
     return(
         <React.Fragment>
-             props.data.map(item=>{
-                return(
-                <h3>{props.fullname}</h3>
-                )
-            })
-            <div className="card-container">
-                <span className="pro" onClick={()=>{handleName(props.fullname)}}>PRO</span>
+            {props.data.map((item, i) => (
+              <div className="card-container" key={i}>
+                <span className="pro" onClick={()=>{handleName(item.fullname)}}>ALERT CLICK</span>
                 <span className="img">{props.children}</span>
-                <h3>{props.fullname}</h3>
-                <h6>{props.profession}</h6>
-                <p>{props.bio}</p>
-            </div>          
+                <h3>{item.fullname}</h3>
+                <h6>{item.profession}</h6>
+                <p>{item.bio}</p>
+            </div> 
+          ))}
+            
         </React.Fragment>
     );
 }
@@ -36,4 +35,6 @@ Profile.defaultProps = {
     bio:PropTypes.string,
     profession:PropTypes.string
   }
+
+  
 export default Profile
